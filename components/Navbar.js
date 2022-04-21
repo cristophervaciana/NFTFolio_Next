c5e56370c9eacfa2f3a050e2b589dcd94f4d42cf
Cristopher Vaciana <cris95vg@outlook.com>
2022-04-21 09:01:03 -0600

    Installing ETH Node

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import React,{useState,useEffect} from 'react';
+const Web3 = require('web3');
 import navStyles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
+
   return (
     <nav className={navStyles.nav}>
       <ul>
